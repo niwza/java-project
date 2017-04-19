5c1f02ee1d7420623c09701cff601a931d42b1e6
Taras Berezovskyi <niwza@svitonline.com>
2017-04-19 21:33:48 +0300

    Added Junit

---
+++ src/RectangleTest.java
@@ -0,0 +1,29 @@
+/**
+ * Created by niwza on 19.04.2017.
+ */
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class RectangleTest {
+    Rectangle myRectangle = new Rectangle(5,6);
+
+    @Test
+    public void testGetArea() {
+        assertEquals(myRectangle.getArea(), 30);
+    }
+
+    @Test
+    public void testGetPerimeter() {
+        assertEquals(myRectangle.getPerimeter(), 22);
+    }
+
+    @Test
+    public void testLength() {
+        assertEquals(myRectangle.length, 5);
+    }
+
+    @Test
+    public void testWidth() {
+        assertEquals(myRectangle.width, 6);
+    }
+}
